b35ebb852a232e639efe6dba703f45cb8a3992a4
DavidCarrilho <luan.david.oliver@gmail.com>
2021-10-05 00:39:08 -0300

    create o layout da home and method buildTextField

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  var initValueController = TextEditingController();
+  var discountController = TextEditingController();
+  var percentualController = TextEditingController();
+  double result = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +24,64 @@ class _MyAppState extends State<MyApp> {
         title: Text("Desconto"),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Divider(),
+              Text(
+                result.toString(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.0,
+                ),
+              ),
+              Divider(),
+              buildTextField('Valor inicial', initValueController),
+              Divider(),
+              buildTextField('Desconto', discountController),
+              Divider(),
+              buildTextField('Inteiro/decimal', percentualController),
+              Divider(),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    result = calcularDesconto(
+                        double.parse(initValueController.text),
+                        double.parse(discountController.text),
+                        int.tryParse(percentualController.text));
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Text(
+                    'Calcular',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
+}
+
+Widget buildTextField(String label, TextEditingController controller) {
+  return TextFormField(
+    controller: controller,
+    decoration: InputDecoration(
+      labelText: label,
+      labelStyle: TextStyle(color: Colors.blue),
+      border: OutlineInputBorder(),
+    ),
+    style: TextStyle(color: Colors.blue, fontSize: 25.0),
+    keyboardType: TextInputType.numberWithOptions(decimal: true)
+  );
 }
 
 double calcularDesconto(double initValue, double discount, int percentual) {
